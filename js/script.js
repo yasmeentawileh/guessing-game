ed064bf1d06a2cba031c1ff59dbf26451c3890ba
Reem Asaad <reemasaad48@gmail.com>
2020-09-08 20:04:36 +0300

    Create a new file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 var userName = prompt('What is your name?');
-alert(userName + ' ,hello to About Me Guessing Game! please answer the questions with yes/no or y/n');
+alert(userName + ', hello to About Me Guessing Game! please answer the questions with yes/no or y/n');
 var nameAnswer = prompt('Is my name Reem? (yes/no or y/n)').toLowerCase();
 if (nameAnswer === 'yes' || nameAnswer === 'y') {
     console.log('You are correct!');
@@ -31,7 +31,182 @@ if (hopeAnswer === 'yes' || hopeAnswer === 'y') {
     alert('You are correct!');
 } 
 
+
 alert('Thank you ' + userName +  ', hope you enjoy it! You will see a short biography about me, hope this information will be enough for you.');
+
+alert('Try to guess a number between 0 to 10, are you ready? please answer the questions with numeric input.');
+alert('You have exactly four opportunities to get the correct answer.');
+var numAttempts = 4;
+var answer = Math.floor(Math.random() * 10 ) + 1;
+console.log(answer);
+while (numAttempts > 0) {
+    var input = prompt('Try to guess!');
+    input = Number (input);
+    console.log(input);
+    
+    numAttempts--;
+
+    if (input < answer) {
+        alert('Your guess is too low');
+    } else if (input > answer) {
+        alert('Your guess is too high.');
+    } else if (input === answer) {
+        alert('Great job, You guessed my number!');
+        break;
+    }
+
+    if (numAttempts == 0) {
+        alert('You didn\'t guess it, the number is ' + answer);
+    }
+}
+
+
+alert('Are you ready for the multiple answers questions? you have 6 attempts for each question.');
+var count = 0;
+var attempts = 6;
+var drinks = ['coffee', 'nescafé', 'tea'];
+for (var i = 0; i < attempts; i++) {
+    var input = prompt('What is my favorite drink?');
+    var userInput = input.toLowerCase();
+    console.log(userInput);
+
+    for (var j = 0; j < drinks.length; j++) {
+        if (input === drinks[j]) {
+            alert('Great job, you got it!');
+            count++;
+            i=6;
+            break;
+        }
+    }
+    if (i !== 6){
+        alert('Wrong, try to guess again!');
+    }
+}
+alert('My favorite drinks are: coffee, nescafé and tea');
+
+var movies = ['romance', 'comedy', 'drama'];
+for (var i = 0; i < attempts; i++) {
+    var input = prompt('What is my favorite genres(types) of movies?');
+    var userInput = input.toLowerCase();
+    console.log(userInput);
+
+    for (var j = 0; j < movies.length; j++) {
+        if (input === movies[j]) {
+            alert('Great job, you got it!');
+            count++;
+            i=6;
+            break;
+        }
+    }
+    if (i !== 6){
+        alert('Wrong, try to guess again!');
+    }
+}
+alert('My favorite genres(types) of movies are: romance, comedy and drama');
+
+var foods = ['pizza', 'burger', 'fried chicken'];
+for (var i = 0; i < attempts; i++) {
+    var input = prompt('What is my favorite junk food?');
+    var userInput = input.toLowerCase();
+    console.log(userInput);
+
+    for (var j = 0; j < foods.length; j++) {
+        if (input === foods[j]) {
+            alert('Great job, you got it!');
+            count++;
+            i=6;
+            break;
+        }
+    }
+    if (i !== 6){
+        alert('Wrong, try to guess again!');
+    }
+}
+alert('My favorite junk foods are: pizza, burger and fried chicken');
+
+var colors = ['red', 'pink', 'black'];
+for (var i = 0; i < attempts; i++) {
+    var input = prompt('What is my favorite color?');
+    var userInput = input.toLowerCase();
+    console.log(userInput);
+
+    for (var j = 0; j < colors.length; j++) {
+        if (input === colors[j]) {
+            alert('Great job, you got it!');
+            count++;
+            i=6;
+            break;
+        }
+    }
+    if (i !== 6){
+        alert('Wrong, try to guess again!');
+    }
+}
+alert('My favorite colors are: red, pink and black');
+
+var countries = ['turkey', 'france', 'italy'];
+for (var i = 0; i < attempts; i++) {
+    var input = prompt('What country would I like to visit?');
+    var userInput = input.toLowerCase();
+    console.log(userInput);
+
+    for (var j = 0; j < countries.length; j++) {
+        if (input === countries[j]) {
+            alert('Great job, you got it!');
+            count++;
+            i=6;
+            break;
+        }
+    }
+    if (i !== 6){
+        alert('Wrong, try to guess again!');
+    }
+}
+alert('The countries are: turkey, france and italy');
+
+var actors = ['lee min ho', 'hyun bin', 'park seo joon'];
+for (var i = 0; i < attempts; i++) {
+    var input = prompt('Who is my favorite Korean actor?');
+    var userInput = input.toLowerCase();
+    console.log(userInput);
+
+    for (var j = 0; j < actors.length; j++) {
+        if (input === actors[j]) {
+            alert('Great job, you got it!');
+            count++;
+            i=6;
+            break;
+        }
+    }
+    if (i !== 6){
+        alert('Wrong, try to guess again!');
+    }
+}
+alert('The favorite Korean actors are: lee min ho, hyun bin and park seo joon');
+
+var flowers = ['jasmine', 'lavender', 'violet'];
+for (var i = 0; i < attempts; i++) {
+    var input = prompt('What is my favorite flower?');
+    var userInput = input.toLowerCase();
+    console.log(userInput);
+
+    for (var j = 0; j < flowers.length; j++) {
+        if (input === flowers[j]) {
+            alert('Great job, you got it!');
+            count++;
+            i=6;
+            break;
+        }
+    }
+    if (i !== 6){
+        alert('Wrong, try to guess again!');
+    }
+}
+alert('My favorite flowers are: jasmine, lavender and violet');
+alert('Your final score: ' + count + '/7');
+
+
+
 
 
 
